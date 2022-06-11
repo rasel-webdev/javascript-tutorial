37fdd7362a72ad7ae7adfee9f78b9b47bd207bf7
Md Mostafizur Rahman <mostafizurhimself@gmail.com>
2022-06-11 16:36:46 +0600

    Add getMax function

--- function.js
+++ function.js
@@ -1,24 +1,34 @@
 // Function Defination or Function Statement
-function add(
-    num1, num2 // parameters
-) {
-    // body...
+function add(num1, num2) {
+    // Add two numbers
     return num1 + num2;
 }
 
-function sub(num1,num2){
+// Substract two numbers
+function sub(num1, num2) {
     return num1 - num2;
-
 }
 
-function multiple (num1,num2){
+// Multiply two numbers
+function multiply(num1, num2) {
     return num1 * num2;
-
 }
-function divide (num1,num2){
+
+// Devide two numbers
+function divide(num1, num2) {
     return num1 / num2;
-
 }
 
+
+// Get max number
+function getMax(num1, num2) {
+    if (num1 > num2) {
+        return num1;
+    } else if (num1 < num2) {
+        return num2;
+    } else {
+        console.log("They are equal");
+    }
+}
 
 
