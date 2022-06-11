a4480784aba3a728d9190f5ae3d0918de193f8bb
Md Rasel Mia <rasel.webdev@gmail.com>
2022-06-11 16:21:15 +0600

    function added

--- function.js
+++ function.js
@@ -6,6 +6,19 @@ function add(
     return num1 + num2;
 }
 
+function sub(num1,num2){
+    return num1 - num2;
+
+}
+
+function multiple (num1,num2){
+    return num1 * num2;
+
+}
+function divide (num1,num2){
+    return num1 / num2;
+
+}
 
 
 
